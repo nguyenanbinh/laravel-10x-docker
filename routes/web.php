5fb2de065904c15e80bd59bf93b97d2d56389d0f
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2023-09-05 19:16:50 +0700

    init project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return "<h1>Hieu Thofn toi choi, bro</h1>";
+});
